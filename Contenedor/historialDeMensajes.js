1aee1e756bb40045304aab1bafb496d3ca56a34a
nahuel di santi <nahuel.disanti@gmail.com>
2022-11-18 19:23:21 -0300

    Agregando las funcines de Historial de mensajeria

---
+++ Contenedor/historialDeMensajes.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const HISTORIAL = './historial.txt'
+
+class Messeges {
+
+    constructor(file) {
+        this.messeges = [];
+        this.file = file
+    }
+
+    saveMessege = (messege) => {
+        try {
+            newMessege = {
+                email: messege.email,
+                messege: messege.messege
+            }
+            const previousMsj = this.getMesseges();
+            previousMsj.push(newMessege);
+            fs.writeFile(this.file, JSON.stringify(this.messeges));
+        }catch(error) {
+            console.log(error)
+        }
+    }
+
+    async getMesseges(){
+        try {
+            const history = JSON.parse(fs.readFile(this.file));
+            return history
+        }catch(error) {
+            console.log(error);
+    }}
+}
